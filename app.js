021aae20046c448bbf6072ac5962d78eec2623ab
saibhavani-git <saibhavani322@gmail.com>
2025-06-09 17:44:30 +0530

    Fix typo in app.js

--- app.js
+++ app.js
@@ -138,7 +138,7 @@ app.get('/logout', (req, res, next) => {
 
 const { GoogleGenerativeAI } = require("@google/generative-ai");
 
-const genAI = new GoogleGenerativeAI("AIzaSyChv_kfTugMSgcKVhjz9KJn3ACXjtI197s");
+const genAI = new GoogleGenerativeAI("");
 const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
 
 app.get('/dsa',isloggedin, async (req, res) => {
